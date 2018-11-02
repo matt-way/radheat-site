ac77df99d4405beb572b87c6dbf9f7912fed809e
Matt Way <matt__way@hotmail.com>
2018-11-02 15:29:40 +1100

    initial site setup

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Index = () => (
+  <div>
+    <Head>
+      <title>Radheat - Sydney Heating Systems</title>
+    </Head>
+    <p>Radheat Content</p>
+  </div>
+)
+
+export default Index
